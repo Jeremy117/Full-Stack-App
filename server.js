a4d704353700be4060cc94490afd6155dde666c5
Jeremy117 <you@example.com>
2018-06-10 06:45:42 -0700

    added routes/api/users.js,profile.js,posts.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB config
@@ -12,6 +16,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/users", profile);
+app.use("/api/users", posts);
 
 const port = process.env.PORT || 5000;
 
